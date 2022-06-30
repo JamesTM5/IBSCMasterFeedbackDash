56370113ea52ee96c233c46a720fb2d365c008dc
JamesTM5 <j.ben.griffin@gmail.com>
2022-06-30 13:07:37 +0100

    Added SchoolDataPrep.R and explanatory text above it

---
+++ SchoolDataPrep.R
@@ -0,0 +1,6 @@
+#This script takes data output from Polinode, and processes it for use in the
+#IBSC Class dashboard.  To use, put the .xlsx output from all the surveys for a
+#given school from polinode into the working directory and then run the script.
+#The output will generate in the working directory the .RDS Files needed by the
+#dashboard. The .xlsx and this file do not need to be published to shinyApps.io
+
