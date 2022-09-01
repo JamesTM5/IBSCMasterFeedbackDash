dc35c3d3fb446b71bf81b39be1c60ad167361947
JamesTMS <j.ben.griffin@gmail.com>
2022-09-01 10:25:53 +0100

    Remove compilation output

--- compilationOutput/assets/R/graphConnected.R
+++
@@ -1,9 +0,0 @@
-#convenience function to find out if a graph is fragmented.
-graphConnected <- function(graph) {
-  CD <- igraph::component_distribution(graph)
-  if(length(CD[!CD==0])>1) {
-    connected <- FALSE
-  } else {
-    connected <- TRUE
-  }
-}
